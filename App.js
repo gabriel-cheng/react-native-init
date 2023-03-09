48e28f2ada545176046218d8306361474479c265
Gabrel Carvalho <gabrel_carvalho.contato@outlook.com>
2023-03-09 15:59:27 -0300

    chore: teste de componente

--- App.js
+++ App.js
@@ -1,18 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, View } from 'react-native';
 
-function TextButton() {
+// Componente react
+function TextButton(props) {
   return(
-    <Text>Pindamonhangaba</Text>
+    <Text>{props.title}</Text>
   );
 }
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello, world!</Text>
+      <Text style={styles.teste}>Hello, world!</Text>
+      <View style={styles.containerTeste}>
+        <Text>Texto 1</Text>
+        <Text>Texto 2</Text>
+        <Text>Texto 3</Text>
+      </View>
       <StatusBar style="auto" />
-      <TextButton />
+      <TextButton title="Titulo maneiro"/>
+      <TextButton title="Outro titulo maneiro"/>
     </View>
   );
 }
@@ -25,6 +32,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   teste: {
+    color: 'green',
+    backgroundColor: 'black'
+  },
+  containerTeste: {
     display: 'flex',
+    flexDirection: 'row'
   }
 });
